be51305d47c30363bc0a12b029a80a739e1cf533
Elaine <chuelaineh@gmail.com>
2018-10-16 16:36:06 -0700

    added Scythe mech

---
+++ src/main/java/frc/mechs/Scythe.java
@@ -0,0 +1,11 @@
+package frc.mechs;
+
+import frc.robot.Mech;
+
+public class Scythe extends Mech {
+
+    public void loop() throws InterruptedException {
+        // code here is repeated continuously while the haunted house is enabled
+        // there is no built-in delay
+    }
+}
