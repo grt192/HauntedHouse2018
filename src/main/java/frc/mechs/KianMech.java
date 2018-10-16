9c452ffaa7d455897466136e08349390ac40d91c
Binh-Minh Nguyen <binhminh.292@outlook.com>
2018-10-16 16:46:50 -0700

    Added Kian Mech zombie Barney

---
+++ src/main/java/frc/mechs/KianMech.java
@@ -0,0 +1,11 @@
+package frc.mechs;
+
+import frc.robot.Mech;
+
+public class KianMech extends Mech {
+
+    public void loop() throws InterruptedException {
+        System.out.println("Make CAD a subgroup");
+        System.out.println("DT better than controls");
+    }
+}
