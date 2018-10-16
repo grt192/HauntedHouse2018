e267a91c04f8c2b526f081f77c809e46a268eccb
Meep3278 <zevgoldhabergordon@gmail.com>
2018-10-16 16:36:17 -0700

    added Eye mech

---
+++ src/main/java/frc/mechs/Eye.java
@@ -0,0 +1,11 @@
+package frc.mechs;
+
+import frc.robot.Mech;
+
+public class Eye extends Mech {
+
+    public void loop() throws InterruptedException {
+        // code here is repeated continuously while the haunted house is enabled
+        // there is no built-in delay
+    }
+}
