ace9dbf4aedbf863192cff2765f14f7637e06c9d
Binh-Minh Nguyen <binhminh.292@outlook.com>
2018-10-16 18:04:17 -0700

    made some code for kian's mech. plz don't kill someone

--- src/main/java/frc/mechs/KianMech.java
+++ src/main/java/frc/mechs/KianMech.java
@@ -1,10 +1,29 @@
 package frc.mechs;
 
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
+import frc.config.Config;
 import frc.robot.Mech;
 
 public class KianMech extends Mech {
 
+    public Solenoid traySolenoid;
+
+    public Timer timer;
+
+    public KianMech() {
+        traySolenoid = new Solenoid(Config.getInt("barneyPneumatic"));
+
+    }
+
+    @Override
     public void loop() throws InterruptedException {
+        if (timer.get() % 2.00 >= 1) {
+            traySolenoid.set(true);
+        } else {
+            traySolenoid.set(false);
+        }
+
         System.out.println("Make CAD a subgroup");
         System.out.println("DT better than controls");
     }
