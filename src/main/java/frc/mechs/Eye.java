fe90254d1e5c94805cd8dee65a7416ecebaeeeca
Meep3278 <zevgoldhabergordon@gmail.com>
2018-10-17 15:29:27 -0700

    Testing Solenoid interactions

--- src/main/java/frc/mechs/Eye.java
+++ src/main/java/frc/mechs/Eye.java
@@ -1,11 +1,22 @@
 package frc.mechs;
 
 import frc.robot.Mech;
+import edu.wpi.first.wpilibj.Solenoid;
+import frc.config.Config;
 
 public class Eye extends Mech {
 
     public void loop() throws InterruptedException {
         // code here is repeated continuously while the haunted house is enabled
         // there is no built-in delay
+        // eye1.set(true);
+        // eye2.set(true);
+        // eye2.set(false);
+        // eye1.set(false);
+    }
+
+    public Eye() {
+        // Solenoid eye1 = new Solenoid(1);
+        // Solenoid eye2 = new Solenoid(1);
     }
 }
