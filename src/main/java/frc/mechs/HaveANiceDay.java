e222204259f47abd2fdfdf1252e9153d70ea8923
InvincibleRMC <rmc@carlstrom.com>
2018-10-17 16:10:43 -0700

    Implemented code.

--- src/main/java/frc/mechs/HaveANiceDay.java
+++ src/main/java/frc/mechs/HaveANiceDay.java
@@ -1,11 +1,25 @@
 package frc.mechs;
 
+import edu.wpi.first.wpilibj.Solenoid;
+import frc.config.Config;
 import frc.robot.Mech;
 
 public class HaveANiceDay extends Mech {
 
+    private Solenoid mechSolenoid;
+
+    public HaveANiceDay() {
+        mechSolenoid = new Solenoid(Config.getInt("have_a_nice_day"));
+    }
+
     public void loop() throws InterruptedException {
         // code here is repeated continuously while the haunted house is enabled
         // there is no built-in delay
+
+        Thread.sleep((long) (5000 + 10000 * Math.random()));
+        mechSolenoid.set(false);
+
+        Thread.sleep((long) (5000 + 10000 * Math.random()));
+        mechSolenoid.set(true);
     }
 }
