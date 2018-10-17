6da8fd81aafb7a39091f8fbec70f3614bbdcf860
Meep3278 <zevgoldhabergordon@gmail.com>
2018-10-17 16:13:01 -0700

    Final commit to mech

--- src/main/java/frc/mechs/Eye.java
+++ src/main/java/frc/mechs/Eye.java
@@ -1,22 +1,31 @@
 package frc.mechs;
 
 import frc.robot.Mech;
+
+import java.util.concurrent.TimeUnit;
+
 import edu.wpi.first.wpilibj.Solenoid;
 import frc.config.Config;
 
 public class Eye extends Mech {
+    private Solenoid eye1;
+    private Solenoid eye2;
+
+    public Eye() {
+        this.eye1 = new Solenoid(Config.getInt("eye_pneumatic_forward"));
+        this.eye2 = new Solenoid(Config.getInt("eye_pneumatic_down"));
+    }
 
     public void loop() throws InterruptedException {
         // code here is repeated continuously while the haunted house is enabled
         // there is no built-in delay
-        // eye1.set(true);
-        // eye2.set(true);
-        // eye2.set(false);
-        // eye1.set(false);
-    }
-
-    public Eye() {
-        // Solenoid eye1 = new Solenoid(1);
-        // Solenoid eye2 = new Solenoid(1);
+        eye1.set(true);
+        Thread.sleep((long) Math.random() * 1000 + 500);
+        eye2.set(true);
+        Thread.sleep((long) Math.random() * 2000 + 1000);
+        eye2.set(false);
+        Thread.sleep((long) Math.random() * 1000 + 500);
+        eye1.set(false);
+        Thread.sleep((long) Math.random() * 10000 + 5000);
     }
 }
