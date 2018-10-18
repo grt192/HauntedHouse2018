659d635eba5d41f518bb7326fc1acde76f78b852
sfshriya <shriyakagolanu@gmail.com>
2018-10-18 15:21:47 -0700

    added face mech

---
+++ src/main/java/frc/mechs/Face.java
@@ -0,0 +1,11 @@
+package frc.mechs;
+
+import frc.robot.Mech;
+
+public class Face extends Mech {
+
+    public void loop() throws InterruptedException {
+        // code here is repeated continuously while the haunted house is enabled
+        // there is no built-in delay
+    }
+}
