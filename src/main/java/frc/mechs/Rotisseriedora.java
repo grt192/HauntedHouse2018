7f5bcd672e86ef773330dd36c56c12441d108ef4
anushkashah <anushkashah654@gmail.com>
2018-10-17 17:56:54 -0700

    Added pneumatics to the code.

--- src/main/java/frc/mechs/Rotisseriedora.java
+++ src/main/java/frc/mechs/Rotisseriedora.java
@@ -1,8 +1,27 @@
 package frc.mechs;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import frc.config.*;
+import edu.wpi.first.wpilibj.Solenoid;
 import frc.robot.Mech;
 
-public class Rotisseriedora extends Mech {
+public class RotisserieDora extends Mech {
+
+    Solenoid mechSolenoid = new Solenoid(Config);
+    TalonSRX mechTalon = new TalonSRX(2);
+
+    public RotisserieDora() {
+        // Config.getInt("RotisserieDora");
+
+        Solenoid mechSolenoid;
+        TalonSRX mechTalon;
+
+        mechSolenoid.set(true);
+        Thread.sleep(5000);
+        mechSolenoid.set(false);
+        mechTalon.set();
+
+    }
 
     public void loop() throws InterruptedException {
         // code here is repeated continuously while the haunted house is enabled
