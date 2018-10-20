97ba62ec029516e34dc33581a9b4f50560e75858
anushkashah <anushkashah654@gmail.com>
2018-10-19 20:06:00 -0700

    Changed code

--- src/main/java/frc/mechs/Rotisseriedora.java
+++ src/main/java/frc/mechs/Rotisseriedora.java
@@ -7,24 +7,24 @@ import frc.robot.Mech;
 
 public class RotisserieDora extends Mech {
 
-    Solenoid mechSolenoid = new Solenoid(Config);
-    TalonSRX mechTalon = new TalonSRX(2);
+    Solenoid mechSolenoid;
+    // TalonSRX mechTalon = new TalonSRX();
 
     private RotisserieDora() {
-        // Config.getInt("RotisserieDora");
-        private void loop() throws InterruptedException {
-            Solenoid mechSolenoid;
-            TalonSRX mechTalon;
-    
-            mechSolenoid.set(true);
-            Thread.sleep(5000);
-            mechSolenoid.set(false);
-            mechTalon.set(0.25);
-            // code here is repeated continuously while the haunted house is enabled
-            // there is no built-in delay
-        }
+        Config.start();
+        mechSolenoid = new Solenoid(Config.getInt("rotisserie_dora"));
+        // Config.defaultConfigTalon(mechTalon);
+    }
 
-        
+    // Config.getInt("RotisserieDora");
+    public void loop() throws InterruptedException {
+
+        mechSolenoid.set(true);
+        Thread.sleep(4000);
+        mechSolenoid.set(false);
+        // mechTalon.set(0.25);
+        // code here is repeated continuously while the haunted house is enabled
+        // there is no built-in delay
     }
 
 }
