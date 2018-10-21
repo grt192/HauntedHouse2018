042879681b4395aad29f911b86333ea9c8a71d45
Cubostar <quinn.arbolante@gmail.com>
2018-10-21 16:42:15 -0700

    Created constructor, figuring out what Config.start() is

--- src/main/java/frc/mechs/AudreySpiders.java
+++ src/main/java/frc/mechs/AudreySpiders.java
@@ -1,18 +1,25 @@
 package frc.mechs;
 
+import frc.config.Config;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import frc.robot.Mech;
 
 public class AudreySpiders extends Mech {
-    private int talon_ID = 15;
-    private TalonSRX motor1 = new TalonSRX(talon_ID);
+
+    private TalonSRX motor1;
+
+    public AudreySpiders() {
+        motor1 = new TalonSRX(Config.getInt("AudreySpiders"));
+        Config.start();
+        motor1.setInverted(true);
+        Config.defaultConfigTalon(motor1);
+    }
 
     public void loop() throws InterruptedException {
         // code here is repeated continuously while the haunted house is enabled
         // there is no built-in delay
-        motor1.setInverted(false);
 
     }
 }
