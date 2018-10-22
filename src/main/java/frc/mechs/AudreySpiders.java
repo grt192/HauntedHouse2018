054518279eac7ff2164577fe01b8a80bb142b656
Cubostar <quinn.arbolante@gmail.com>
2018-10-21 22:44:37 -0700

    Edited to use PercentOutput and not current limits

--- src/main/java/frc/mechs/AudreySpiders.java
+++ src/main/java/frc/mechs/AudreySpiders.java
@@ -1,6 +1,8 @@
 package frc.mechs;
 
 import frc.config.Config;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
@@ -11,46 +13,47 @@ public class AudreySpiders extends Mech {
     private TalonSRX motor1;
 
     // Need to test mech to figure out variables
-    // currentAmount must be greater than 5 amps
-    private int currentAmount;
-    private int currentPeak;
-    private int peakDuration;
-    private int timeUp;
-    private int timeDown;
+    private int outputPercent;
+    private long timeUp;
+    private long timeDown;
+    private long currentTime = System.currentTimeMillis();
 
     public AudreySpiders() {
 
         motor1 = new TalonSRX(Config.getInt("AudreySpiders"));
-        Config.start();
         motor1.setNeutralMode(NeutralMode.Brake);
-        motor1.enableCurrentLimit(true);
+
     }
 
     public void spiderDown() {
 
-        peakDuration = 50;
-
         motor1.setInverted(false);
-        motor1.configContinuousCurrentLimit(currentAmount, 0);
-        motor1.configPeakCurrentLimit(currentPeak, 0);
-        motor1.configPeakCurrentDuration(peakDuration, 0);
+        motor1.set(ControlMode.PercentOutput, outputPercent);
+
     }
 
     public void spiderUp() {
 
-        peakDuration = 50;
-
         motor1.setInverted(true);
-        motor1.configContinuousCurrentLimit(currentAmount, 0);
-        motor1.configPeakCurrentLimit(currentPeak, 0);
-        motor1.configPeakCurrentDuration(peakDuration, 0);
+        motor1.set(ControlMode.PercentOutput, outputPercent);
+
     }
 
     public void loop() throws InterruptedException {
         // code here is repeated continuously while the haunted house is enabled
         // there is no built-in delay
 
-        // can use timeUp, timeDown, and wait() to movie spider
+        // Can use timeUp, timeDown, currentTime, and wait() to movie spider
+        // Will make more complex once figuring out variables
+
+        // while(System.currentTimeMillis() < timeDown){
+        // spiderDown();
+        // wait(2000);
+        // }
+        // while(System.currentTimeMillis() < timeUp){
+        // spiderUp();
+        // wait(2000);
+        // }
 
     }
 }
