83c8b637a17f7657d08fb69fe179b30b07de097b
Ethan <elowenthal16@student.hausner.com>
2018-10-23 14:48:34 -0700

    added soft limits

--- src/main/java/frc/mechs/TurningHeadOpenLoop.java
+++ src/main/java/frc/mechs/TurningHeadOpenLoop.java
@@ -5,14 +5,20 @@ import frc.config.Config;
 import frc.robot.JeVois;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class TurningHeadOpenLoop extends Mech {
 
+    private final int encoderTicks = 4096 * 100; // ticks per rotaion * motor to encoder ratio
+    private int encoderOffset;
+
     private TalonSRX rotateMotor;
+
     private double facePos;
     private JeVois camera;
+
     Double percentOutput;
 
     public TurningHeadOpenLoop(JeVois faceCam) {
@@ -25,6 +31,20 @@ public class TurningHeadOpenLoop extends Mech {
         // too quikly
 
         rotateMotor.setNeutralMode(NeutralMode.Brake); // stop when power is dropped
+        rotateMotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 30); // https://www.vexrobotics.com/217-5046.html
+
+        findZero();
+    }
+
+    private void findZero() {
+        encoderOffset = rotateMotor.getSelectedSensorPosition(0); // make the head face forward
+
+        float limit = encoderTicks / 4; // TODO: find value (how far the head can turn in each direction)
+
+        rotateMotor.configForwardSoftLimitThreshold(Math.round(limit - encoderOffset), 0);
+        rotateMotor.configReverseSoftLimitThreshold(Math.round(-limit + encoderOffset), 0);
+        rotateMotor.configForwardSoftLimitEnable(true, 0);
+        rotateMotor.configReverseSoftLimitEnable(true, 0);
     }
 
     private void moveHead(Double targetPosition) {
