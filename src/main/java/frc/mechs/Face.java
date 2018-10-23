f02e7b9ea38da3570837659c2c2d73b0f71e6adf
sfshriya <shriyakagolanu@gmail.com>
2018-10-22 23:37:33 -0700

    finished code for hand movement with configurable delay time

--- src/main/java/frc/mechs/Face.java
+++ src/main/java/frc/mechs/Face.java
@@ -1,10 +1,27 @@
 package frc.mechs;
 
 import frc.robot.Mech;
+import frc.config.*;
+import edu.wpi.first.wpilibj.Solenoid;
+import frc.robot.Mech;
 
 public class Face extends Mech {
 
+    Solenoid mechSolenoid;
+    int handDelay;
+
+    private Face() {
+        Config.start();
+        mechSolenoid = new Solenoid(Config.getInt("face"));
+        handDelay = Config.getInt("face.delay");
+    }
+
     public void loop() throws InterruptedException {
+        mechSolenoid.set(true);
+        Thread.sleep(handDelay);
+        mechSolenoid.set(false);
+        Thread.sleep(handDelay);
+
         // code here is repeated continuously while the haunted house is enabled
         // there is no built-in delay
     }
