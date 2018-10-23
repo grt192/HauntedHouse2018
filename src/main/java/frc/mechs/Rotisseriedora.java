21ecd1c4b88424d9f4b28d68ed4c15e4540188df
anushkashah <anushkashah654@gmail.com>
2018-10-22 23:22:15 -0700

    Added my mech to mechs HashSet.

--- src/main/java/frc/mechs/Rotisseriedora.java
+++ src/main/java/frc/mechs/Rotisseriedora.java
@@ -11,7 +11,7 @@ public class RotisserieDora extends Mech {
     Solenoid mechSolenoid;
     TalonSRX mechTalon;
 
-    private RotisserieDora() {
+    public RotisserieDora() {
         Config.start();
         mechSolenoid = new Solenoid(Config.getInt("rotisserie_dora"));
         mechTalon = new TalonSRX(Config.getInt("rotisserie_dora"));
@@ -29,6 +29,7 @@ public class RotisserieDora extends Mech {
         mechSolenoid.set(true);
         Thread.sleep(4000);
         mechSolenoid.set(false);
+        Thread.sleep(3000);
         // code here is repeated continuously while the haunted house is enabled
         // there is no built-in delay
     }
